2adf43850dd256e337b97cdbe10cc01e073f9b51
Son2kwon <paulson01@naver.com>
2023-11-27 13:48:09 +0900

    <Dice> 1. roll() method 수정 2. getNumber() 추가
    <Turn>
    1. turnStart() method 구현
    
    <Game>
    1. turnStart() method 구현 중

--- OOP_Project_4/Dice.h
+++ OOP_Project_4/Dice.h
@@ -14,29 +14,14 @@ public:
 	void roll() {	// 주사위를 굴림
 		srand(time(NULL));	// 난수를 위한 seed 설정
 
-		switch (rand() % 6 + 1) {	// 숫자는 1 ~ 6
-		case 1:
-			number = 1;
-			break;
-		case 2:
-			number = 2;
-			break;
-		case 3:
-			number = 3;
-			break;
-		case 4:
-			number = 4;
-			break;
-		case 5:
-			number = 5;
-			break;
-		case 6:
-			number = 6;
-			break;
-		}
+		number = (rand() % 6) + 1;	// number에 1 ~ 6 중 하나를 random하게 저장
 	}
 
 	void initialize() {
 		number = 0;
+	}
+
+	int getNumber() {
+		return number;
 	}
 };
